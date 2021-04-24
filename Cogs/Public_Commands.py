669a7b5d9e36dd367f27d66d0038e7d9f372560b
BuildTools <unconfigured@null.spigotmc.org>
2021-04-25 00:32:04 +0200

    Added website command

--- Cogs/Public_Commands.py
+++ Cogs/Public_Commands.py
@@ -1,3 +1,5 @@
+import random
+
 import discord.ext.commands
 from discord.ext import commands
 from discord.utils import get
@@ -61,9 +63,10 @@ class Public(commands.Cog, name="Available for everyone"):
         if auth.db.is_token_auth(str(ctx.message.author.id)):
             address = auth.db.get_address(str(ctx.message.author.id))
             await ctx.message.author.send(
-                "You are already logged with {} ({})\nYou can logout using `{}logout`".format(address,
-                                                                                              auth.db.get_address_status(
-                                                                                                  address), ctx.prefix))
+                "You are already logged with {} ({})\n"
+                "You can logout using `{}logout`".format(address,
+                                                         auth.db.get_address_status(
+                                                             address), ctx.prefix))
             return
         auth.get_dna_url(token=str(ctx.message.author.id))
         await ctx.message.author.send(
@@ -88,3 +91,19 @@ class Public(commands.Cog, name="Available for everyone"):
         if isinstance(error, commands.CommandOnCooldown):
             await ctx.reply(f"Stop Spamming! Try again in {error.retry_after:.2f}s.",
                             delete_after=15)
+
+    @commands.command(name="website",
+                      brief="Offical Website",
+                      help="Sends you a link to the offical webpage",
+                      aliases=["link", "page", "webpage", "donate"])
+    async def website(self, ctx: commands.Context):
+        embed = discord.Embed(color=discord.Colour(random.randint(0, 16777215)))
+        embed.title = "Links"
+        embed.add_field(name="Website", value="https://www.idena.io/", inline=True)
+        embed.add_field(name="Discord", value="https://discord.gg/96Sw2e6", inline=True)
+        embed.add_field(name="Bots Github", value="https://github.com/iyomisc/idenauth", inline=True)
+        embed.add_field(name="Donate to bot devs",
+                        value="iyomisc: 0xf2cc549874f366b66b11eb7bb3ad5a66343ce369 \n "
+                              "The_Bow_Hunter: 0x590a5c6002f45e26cf8d818299ce35925e5f6732")
+
+        await ctx.reply(embed=embed, delete_after=40)
